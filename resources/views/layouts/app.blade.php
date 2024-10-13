87126cfbe46dae52b010ed129269a31466a75703
jrgavilanes <jrgavilanes@gmail.com>
2024-10-13 12:14:52 +0200

    name footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,6 +42,7 @@
 
         @yield('content')
 
+        {{-- footer --}}
         <div class="h-[1px] bg-white/5 sm:w-[800px]"></div>
         <p class="text-white text-xs mt-2">
             <a href="{{ route('dashboard') }}">- admin -</a>
